ec3c0906877e7acf06f17a288bb2023fdbba3080
Hruday <hrudayranjan8@gmail.com>
2023-02-07 08:56:52 +0530

    feat:alert

--- src/components/EarthquakesPosition.jsx
+++ src/components/EarthquakesPosition.jsx
@@ -1,9 +1,10 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Map from "./Map";
 
 export default function EarthquakesPosition() {
-  const [data, setData] = useState(null);
+  const [tempData, setTempData] = useState(null);
+  const [dataToPrintPointer, setDataToPrintPointer] = useState(null);
   const oneDayInMs = 24 * 60 * 60 * 1000;
   const today = new Date();
   const [selectedStartDate, setSelectedStartDate] = useState(
@@ -11,54 +12,73 @@ export default function EarthquakesPosition() {
   );
   const [selectedEndDate, setSelectedEndDate] = useState(new Date());
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      fetch(
-        `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${new Date(
-          selectedStartDate
-        )
-          .toISOString()
-          .substr(0, 10)}&endtime=${new Date(selectedEndDate)
-          .toISOString()
-          .substr(0, 10)}&minmagnitude=1`
+  const getData = async () => {
+    fetch(
+      `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${new Date(
+        selectedStartDate
       )
-        .then((response) => response.json())
-        .then((data) => setData(data));
+        .toISOString()
+        .substr(0, 10)}&endtime=${new Date(selectedEndDate)
+        .toISOString()
+        .substr(0, 10)}&minmagnitude=1`
+    )
+      .then((response) => response.json())
+      .then((data) => setTempData(data));
+  };
+
+  useEffect(() => {
+    // getData();
+
+    const intervalId = setInterval(() => {
+      getData();
     }, 15000);
-
     return () => clearInterval(intervalId);
-  }, [selectedStartDate]);
+  }, [tempData]);
 
-  let outPut = data?.features[20];
-  data?.features.map((itm) => {
-    // console.log(itm.properties, "data");
-  });
+  const onSubmitHandler = async () => {
+    await getData();
+    setDataToPrintPointer(tempData);
+
+    console.log(tempData, "tempData");
+  };
 
   return (
     <div>
-      <TextField
-        id="date"
-        label="Date"
-        type="date"
-        value={selectedStartDate.toISOString().substr(0, 10)}
-        onChange={(event) => setSelectedStartDate(new Date(event.target.value))}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        sx={{ mr: 2, mb: 2, mt: 2 }}
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <TextField
+          id="date"
+          label="Date"
+          type="date"
+          size="small"
+          value={selectedStartDate.toISOString().substr(0, 10)}
+          onChange={(event) =>
+            setSelectedStartDate(new Date(event.target.value))
+          }
+          InputLabelProps={{
+            shrink: true,
+          }}
+          sx={{ mr: 2, mb: 2, mt: 2 }}
+        />
+        <TextField
+          id="date"
+          label="Date"
+          type="date"
+          size="small"
+          value={selectedEndDate.toISOString().substr(0, 10)}
+          onChange={(event) => setSelectedEndDate(new Date(event.target.value))}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          sx={{ mb: 2, mt: 2, mr: 2 }}
+        />
+        <Button variant="contained" onClick={onSubmitHandler}>
+          Submit
+        </Button>
+      </div>
+      <Map
+        points={dataToPrintPointer?.features}
+        tempPoints={tempData?.features}
       />
-      <TextField
-        id="date"
-        label="Date"
-        type="date"
-        value={selectedEndDate.toISOString().substr(0, 10)}
-        onChange={(event) => setSelectedEndDate(new Date(event.target.value))}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        sx={{ mb: 2, mt: 2 }}
-      />
-      <Map points={data?.features} />
     </div>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,33 +4,25 @@ import {
   FeatureGroup,
   MapContainer,
   Marker,
-  Popup,
   TileLayer,
   Tooltip,
 } from "react-leaflet";
 import iconMarker from "leaflet/dist/images/marker-icon.png";
 import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-export default function Map({ points }) {
+export default function Map({ points, tempPoints }) {
+  // console.log(points, "Points");
   L.Icon.Default.imagePath = "leaflet_images/";
 
   const icon = L.icon({
     iconRetinaUrl: iconRetina,
     iconUrl: iconMarker,
-    // shadowUrl: iconShadow,
     iconSize: [20, 20],
   });
 
-  const center = [
-    [39.4093333, -123.2663333],
-    [33.648, -116.7418333],
-    [33.6528333, -116.7195],
-    [32.2869873, -101.7752001],
-  ];
-  const fillBlueOptions = { fillColor: "blue" };
+  const compareObjects = (a, b) => a.id === b.id;
   const purpleOptions = { color: "purple" };
 
   return (
@@ -44,46 +36,36 @@ export default function Map({ points }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* 
-      {points?.map((elm, i) => {
-        return (
-          <Circle
-            center={[elm.geometry.coordinates[1], elm.geometry.coordinates[0]]}
-            pathOptions={fillBlueOptions}
-            radius={600}
-            key={i}
-          />
-        );
-      })} */}
 
-      {/* {points?.map((elm, i) => {
-        return (
-          <FeatureGroup
-            pathOptions={`${
-              elm.properties.alert ? purpleOptions : fillBlueOptions
-            }`}
-            key={i}
-          >
-            <Tooltip sticky>
-              <p>
-                <b>Title:</b> {elm.properties.title}
-              </p>
-              <p>Magnitude: {elm.properties.mag}</p>
-              <p>Place: {elm.properties.place}</p>
-              <p>Type: {elm.properties.type}</p>
-              <p>Time: {elm.properties.updated}</p>
-            </Tooltip>
-
-            <Circle
-              center={[
-                elm.geometry.coordinates[1],
-                elm.geometry.coordinates[0],
-              ]}
-              radius={`${elm.properties.alert ? 800 : 400}`}
-            />
-          </FeatureGroup>
-        );
-      })} */}
+      {tempPoints
+        ?.filter(
+          (item2) => !points?.find((item1) => compareObjects(item1, item2))
+        )
+        .map((elm, i) => {
+          return (
+            <FeatureGroup pathOptions={purpleOptions} key={i}>
+              <Tooltip sticky>
+                <p>
+                  <b>Title:</b> {elm.properties.title}
+                </p>
+                <p>Magnitude: {elm.properties.mag}</p>
+                <p>Place: {elm.properties.place}</p>
+                <p>Type: {elm.properties.type}</p>
+                <p>
+                  Time:{" "}
+                  {new Date(elm.properties.updated).toISOString().split("T")[0]}{" "}
+                </p>
+              </Tooltip>
+              <Circle
+                center={[
+                  elm.geometry.coordinates[1],
+                  elm.geometry.coordinates[0],
+                ]}
+                radius={1000}
+              />
+            </FeatureGroup>
+          );
+        })}
 
       {points?.map((elm, i) => {
         return (
@@ -99,19 +81,22 @@ export default function Map({ points }) {
               <p>
                 <b>Title:</b> {elm.properties.title}
               </p>
-              <p>Magnitude: {elm.properties.mag}</p>
-              <p>Place: {elm.properties.place}</p>
+              <p>
+                <b>Magnitude:</b> {elm.properties.mag}
+              </p>
+              <p>
+                <b>Place:</b> {elm.properties.place}
+              </p>
               <p>Type: {elm.properties.type}</p>
-              <p>Time: {elm.properties.updated}</p>
+              <p>
+                Time:{" "}
+                {new Date(elm.properties.updated).toISOString().split("T")[0]}
+              </p>
               <p>Aert: {elm.properties.alert === null ? "No" : "Yes"}</p>
             </Tooltip>
           </Marker>
         );
       })}
-
-      {/* <Marker position={[51.505, -0.09]} icon={icon}>
-        <Popup>Position 51.505, -0.09</Popup>
-      </Marker> */}
     </MapContainer>
   );
 }
